2fb7a2ed88f4dd7d7e3fe0f42195848eae25c2b8
nexus-uw <nexus-uw@users.noreply.github.com>
2022-10-22 16:46:16 -0700

    test parsing the body before signing
    (maybe the signature doesnt work with base64 encoded body?)

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { RequestSigner } from 'aws4'
 import { CloudFrontRequestEvent, Callback, Context } from 'aws-lambda'
+import { Buffer } from 'node:buffer'
 
 export const handler = (
     event: CloudFrontRequestEvent,
@@ -8,7 +9,7 @@ export const handler = (
 ) => {
     const request = event.Records[0].cf.request
     delete request.headers['x-forwarded-for'] // fails signature verification if included
-    delete request.headers['content-length'] // causing failures on post/put ...
+    //delete request.headers['content-length'] // causing failures on post/put ...
     // assumes <jib>.lambda-url.<region>.on.aws
     const region = request.origin!.custom?.domainName.split('.')[2]
 
@@ -24,7 +25,9 @@ export const handler = (
             headers[key] = request.headers[key][0].value
             return headers
         }, {} as { [l: string]: string }),
-        body: request.body?.data
+        body:request.body ?
+         Buffer.from(request.body?.data, request.body?.encoding === 'base64'?'base64':'ascii') // should always be base64 https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/edge-functions-restrictions.html
+         : ""
     })
 
     const { headers } = signer.sign()
