4b562658a1773c24f3837e967a010028e065d100
Simon Ramsay <nexus-uw@users.noreply.github.com>
2022-10-23 14:55:42 -0700

    fixed POST/PUT signatures
    -parse the body base64 body to a buffer before signing (the signature didnt work with the base64 encoded body)
    - dont try to parse body if not POST/PUT
    - (kept) delete content-length header (to let it be recalculated) -> this may break things if body is very large

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { RequestSigner } from 'aws4'
 import { CloudFrontRequestEvent, Callback, Context } from 'aws-lambda'
+import { Buffer } from 'node:buffer'
 
 export const handler = (
     event: CloudFrontRequestEvent,
@@ -24,7 +25,9 @@ export const handler = (
             headers[key] = request.headers[key][0].value
             return headers
         }, {} as { [l: string]: string }),
-        body: request.body?.data
+        body:['POST','PUT'].includes(request.method) && request.body?.data ?
+         Buffer.from(request.body?.data, request.body?.encoding === 'base64'?'base64':'ascii') // should always be base64 https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/edge-functions-restrictions.html
+         : ""
     })
 
     const { headers } = signer.sign()
